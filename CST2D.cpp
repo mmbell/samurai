697c0335c832d982289f27980addac0e8800087e
Michael Bell <michaelmbell@gmail.com>
2009-08-13 19:43:36 +0000

    Global change of tcvar to samurai

---
+++ CST2D.cpp
@@ -0,0 +1,18 @@
+/*
+ *  CST2D.cpp
+ *  tcvar
+ *
+ *  Created by Michael Bell.
+ *  Copyright 2009. All rights reserved.
+ *
+ */
+
+#include "CST2D.h"
+
+CST2D::CST2D()
+{
+}
+
+CST2D::~CST2D()
+{
+}
