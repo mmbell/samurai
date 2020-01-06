d21dcea36cae0b9a5c23862fd6485e8b4bbb5685
mike-dixon <dixon@ucar.edu>
2020-01-05 19:04:43 -0700

    cmake/Modules/FindGeographicLib.cmake - (a) adding /usr/local/lrose/lib to PATHS. (b) Copying simpler version over from fractl

--- cmake/Modules/FindGeographicLib.cmake
+++ cmake/Modules/FindGeographicLib.cmake
@@ -1,42 +1,37 @@
-find_path(GeographicLib_INCLUDE_DIR NAMES GeographicLib/Config.h DOC "GeographicLib include directory")
-find_library(GeographicLib_LIBRARY_RELEASE NAMES Geographic DOC "GeographicLib library (release)")
-find_library(GeographicLib_LIBRARY_DEBUG NAMES Geographic_d DOC "GeographicLib library (debug)")
+# Look for GeographicLib
+#
+# Sets
+#  GeographicLib_FOUND = TRUE
+#  GeographicLib_INCLUDE_DIRS = /usr/local/include
+#  GeographicLib_LIBRARIES = /usr/local/lib/libGeographic.so
+#  GeographicLib_LIBRARY_DIRS = /usr/local/lib
 
-include(SelectLibraryConfigurations)
-select_library_configurations(GeographicLib)
+find_library (GeographicLib_LIBRARIES Geographic
+  PATHS "${CMAKE_INSTALL_PREFIX}/../GeographicLib/lib" /usr/local/lrose/lib )
 
-if(GeographicLib_INCLUDE_DIR)
-    file(STRINGS ${GeographicLib_INCLUDE_DIR}/GeographicLib/Config.h _config_version REGEX "GEOGRAPHICLIB_VERSION_STRING")
-    string(REGEX MATCH "\"([0-9.]+)\"$" _match_version ${_config_version})
-    set(GeographicLib_VERSION_STRING ${CMAKE_MATCH_1})
-endif()
+  if (GeographicLib_LIBRARIES)
+  get_filename_component (GeographicLib_LIBRARY_DIRS
+    "${GeographicLib_LIBRARIES}" PATH)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GeographicLib
-    REQUIRED_VARS GeographicLib_INCLUDE_DIR GeographicLib_LIBRARY
-    FOUND_VAR GeographicLib_FOUND
-    VERSION_VAR GeographicLib_VERSION_STRING)
+  get_filename_component (_ROOT_DIR "${GeographicLib_LIBRARY_DIRS}" PATH)
+  set (GeographicLib_INCLUDE_DIRS "${_ROOT_DIR}/include")
+  set (GeographicLib_BINARY_DIRS "${_ROOT_DIR}/bin")
+  if (NOT EXISTS "${GeographicLib_INCLUDE_DIRS}/GeographicLib/Config.h")
+    get_filename_component(_ROOT_DIR "${_ROOT_DIR}" PATH)                     # Added to script
+    set (GeographicLib_INCLUDE_DIRS "${_ROOT_DIR}/include")                   # Added to script
+    set (GeographicLib_BINARY_DIRS "${_ROOT_DIR}/bin")                        # Added to script
+    if (NOT EXISTS "${GeographicLib_INCLUDE_DIRS}/GeographicLib/Config.h")    # Added to script
+      unset (GeographicLib_INCLUDE_DIRS)
+      unset (GeographicLib_LIBRARIES)
+      unset (GeographicLib_LIBRARY_DIRS)
+            unset (GeographicLib_BINARY_DIRS)
+    endif()                                                                   # Added to script
+  endif ()
+  unset (_ROOT_DIR)                                                           # Moved below if() statements
+endif ()
 
-if(GeographicLib_FOUND AND NOT TARGET GeographicLib::GeographicLib)
-    add_library(GeographicLib::GeographicLib UNKNOWN IMPORTED)
-    set_target_properties(GeographicLib::GeographicLib PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${GeographicLib_INCLUDE_DIR}"
-        IMPORTED_LOCATION "${GeographicLib_LIBRARY}"
-        IMPORTED_LINK_INTERFACE_LANGUAGES "CXX")
-    if(EXISTS "${GeographicLib_LIBRARY_RELEASE}")
-        set_property(TARGET GeographicLib::GeographicLib APPEND PROPERTY
-            IMPORTED_CONFIGURATIONS RELEASE)
-        set_target_properties(GeographicLib::GeographicLib PROPERTIES
-            IMPORTED_LOCATION_RELEASE "${GeographicLib_LIBRARY_RELEASE}")
-    endif()
-    if(EXISTS "${GeographicLib_LIBRARY_DEBUG}")
-        set_property(TARGET GeographicLib::GeographicLib APPEND PROPERTY
-            IMPORTED_CONFIGURATIONS DEBUG)
-        set_target_properties(GeographicLib::GeographicLib PROPERTIES
-            IMPORTED_LOCATION_DEBUG "${GeographicLib_LIBRARY_DEBUG}")
-    endif()
-endif()
-
-mark_as_advanced(GeographicLib_INCLUDE_DIR GeographicLib_LIBRARY)
-set(GeographicLib_INCLUDE_DIRS ${GeographicLib_INCLUDE_DIR})
-# GeographicLib_LIBRARIES is set by SelectLibraryConfigurations
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args (GeographicLib DEFAULT_MSG
+  GeographicLib_LIBRARY_DIRS GeographicLib_LIBRARIES GeographicLib_INCLUDE_DIRS)
+mark_as_advanced (GeographicLib_LIBRARY_DIRS GeographicLib_LIBRARIES
+  GeographicLib_INCLUDE_DIRS)
