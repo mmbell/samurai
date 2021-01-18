f397216003d5d60da2c5b6601eda9f4243de0268
mike-dixon <dixon@ucar.edu>
2021-01-17 19:18:47 -0700

    Preparing for elle final release. Modified cmake to match fract and vortrac setup. Added src/CMakeLists.txt to move logic to src dir. Changed cmake/Modules to cmake/modules. Tested use of tdrp_gen if paramdef file is modified.

---
+++ cmake/modules/lrose-config.cmake
@@ -0,0 +1,43 @@
+# User can tell us where to look
+
+set(LROSE_PREFIX $ENV{LROSE_INSTALL_DIR})
+
+# If not, use the default location
+if(NOT LROSE_PREFIX)
+  set(LROSE_PREFIX "/usr/local/lrose")
+endif(NOT LROSE_PREFIX )
+
+set(LROSE_LIBRARIES
+  kd
+  tdrp
+  Radx
+  Ncxx
+  toolsa
+  netcdf
+  hdf5_cpp
+  hdf5
+  )
+
+find_program(TDRP_EXECUTABLE tdrp_gen PATHS ${LROSE_BIN_DIR} /usr/local/bin )
+
+# Add a custom generator for TDRP Params.cc and Params.hh files
+# from their associated paramdef.<app> file
+
+find_program(TDRP_EXECUTABLE tdrp_gen PATHS ${LROSE_BIN_DIR} /usr/local/bin )
+
+add_custom_command(
+  OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/Params.hh ${CMAKE_CURRENT_SOURCE_DIR}/Params.cc
+  DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/paramdef.${PROJECT_NAME}
+  COMMAND cd ${CMAKE_CURRENT_SOURCE_DIR} && ${TDRP_EXECUTABLE}
+  -c++
+  -f paramdef.${PROJECT_NAME}
+  -prog ${PROJECT_NAME}
+  -add_ncar_copyright
+  COMMENT "Generating/updating Params.hh and Params.cc for ${PROJECT_NAME}"
+  )
+
+message(STATUS "lrose-config: CMAKE_INSTALL_PREFIX: " ${CMAKE_INSTALL_PREFIX} )
+message(STATUS "lrose-config: LROSE_INSTALL_DIR: " ${LROSE_INSTALL_DIR} )
+message(STATUS "lrose-config: LROSE_LIBRARIES: " ${LROSE_LIBRARIES} )
+message(STATUS "lrose-config: TDRP_EXECUTABLE: " ${TDRP_EXECUTABLE} )
+
