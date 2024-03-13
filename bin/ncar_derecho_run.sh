b778769168600847c4fe8eaba6df693b3f4c365a
Jian Sun <sjsprecious@gmail.com>
2024-03-13 14:01:26 -0600

    update runscript for Derecho at NCAR; test for CPU only

---
+++ bin/ncar_derecho_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ./build/release/bin/samurai does not work somehow
+cd build
+EXE="./release/bin/samurai"
+
+# Determine if we're running on the GPU or not by checking the libraries we've linked:
+GPU=$(grep -c libacc ${EXE})
+
+module list
+
+if [ "${GPU}" == "0" ]; then
+  echo "CPU run; using one cpu core"
+else
+  echo "GPU run; using one GPU"
+fi
+
+${EXE} $*
