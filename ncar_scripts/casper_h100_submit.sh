7d4a3084fcdc3d3586901efb112b5cb666835a0d
Mark Onders <109011127+markonders@users.noreply.github.com>
2024-07-03 12:56:46 -0600

    Create casper_h100_submit.sh
    Create Casper H100 run script

---
+++ ncar_scripts/casper_h100_submit.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -l
+#PBS -N SAMURAI
+#PBS -A NEOL0013
+#PBS -l select=1:ncpus=36:ompthreads=1:mem=700GB:ngpus=1
+#PBS -l gpu_type=h100
+#PBS -q casper
+#PBS -l walltime=05:00:00
+#PBS -j oe
+#PBS -k eod
+ 
+cd $PBS_O_WORKDIR
+cd ..
+export SAMURAI_ROOT=$(pwd)
+
+ID=`date '+%Y%m%d%H%M'`
+##################
+# Build the code #
+##################
+# Use cc90 for h100 GPU on Casper
+sed -i 's/cc80/cc90/g' CMakeLists.txt
+sed -i 's/cc70/cc90/g' CMakeLists.txt
+
+cd ncar_scripts 
+./ncar_build.sh gpu
+
+##############
+# Run a case #
+##############
+suffix="casper_gpu"
+for i in beltrami supercell hurricane typhoonChanthu2020  # hurricane_4panel
+
+do
+  ./ncar_run.sh $SAMURAI_ROOT/ncar_scripts/TDRP/${i}.tdrp >& log_${i}_$suffix.$ID
+  if [ ! -d  ${i}_${suffix} ]; then
+     mkdir ${i}_${suffix}
+  fi
+  mv $SAMURAI_ROOT/run/timing.0 ${i}_${suffix}/timing.$ID
+  mv $SAMURAI_ROOT/run/samurai* log* ${i}_${suffix}
+done
