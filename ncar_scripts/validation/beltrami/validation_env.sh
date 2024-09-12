f34d1e5f0f8791d63f256f2be1411f7b904ceb48
cenamiller <cena.miller@gmail.com>
2024-09-12 09:26:59 -0600

    optimizing python library install and deleting unnecessary data to create space on cloud resource

--- ncar_scripts/validation/beltrami/validation_env.sh
+++ ncar_scripts/validation/beltrami/validation_env.sh
@@ -3,9 +3,18 @@
 # Set the working directory
 cd /app
 
+# Copy necessary output for validation and delete rest to make space
+cp /app/samurai/build/release/bin/samurai_XYZ_wind_analysis.nc /app/samurai/ncar_scripts/validation/beltrami/samurai_XYZ_wind_analysis_ref.nc
+rm -rf /app/samurai/build
+rm -rf /app/data
+
 # Ensure pip is installed
-apt-get update && apt-get install --reinstall zlib1g
-apt-get install -y python3-venv
+apt-get update && \
+apt-get install --reinstall -y zlib1g python3-venv && \
+apt-get clean && \
+rm -rf /var/lib/apt/lists/*
+
+# Create a virtual environment and install necessary packages
 python3 -m venv /app/venv && \
     /app/venv/bin/pip install --upgrade pip && \
     /app/venv/bin/pip install numpy xarray netCDF4 scipy cftime 
@@ -14,10 +23,8 @@ python3 -m venv /app/venv && \
 chmod +x /app/samurai/ncar_scripts/validation/beltrami/bel_val_nc.py
 
 echo "#!/bin/bash" > /app/run_validation.sh
-echo "cd /app/samurai/build/release/bin/" >> /app/run_validation.sh
-echo "./samurai -params /app/samurai/ncar_scripts/TDRP/beltrami.tdrp | tee /app/samurai/ncar_scripts/validation/beltrami/test_log" >> /app/run_validation.sh
 echo "cd /app/samurai/ncar_scripts/validation/beltrami/" >> /app/run_validation.sh
-echo "/app/venv/bin/python3 bel_val_nc.py samurai_XYZ_wind_analysis_ref.nc /app/samurai/build/release/bin/samurai_XYZ_wind_analysis.nc" >> /app/run_validation.sh
+echo "/app/venv/bin/python3 bel_val_nc.py samurai_XYZ_wind_analysis_ref.nc samurai_XYZ_wind_analysis.nc" >> /app/run_validation.sh
 
 chmod +x /app/run_validation.sh
 
