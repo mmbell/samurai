20f510df01bd536809b8cdc633c3214af4980205
halehawk <haiyingx@ucar.edu>
2024-10-13 19:05:43 -0600

    change to main queue and run on all tests

--- ncar_scripts/derecho_cpu_submit.sh
+++ ncar_scripts/derecho_cpu_submit.sh
@@ -24,7 +24,7 @@ cd ncar_scripts
 # Run a case #
 ##############
 suffix="derecho_cpu"
-for i in beltrami #supercell hurricane typhoonChanthu2020 # hurricane_4panel
+for i in beltrami supercell hurricane typhoonChanthu2020 # hurricane_4panel
 do
   ./ncar_run.sh $SAMURAI_ROOT/ncar_scripts/TDRP/${i}.tdrp >& log_${i}_$suffix.$ID
   echo "log_${i}_$suffix.$ID is done"
