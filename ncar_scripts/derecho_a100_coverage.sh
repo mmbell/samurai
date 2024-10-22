ede4436c31083ea8f7a6fb1f1aeef0dde072b539
halehawk <haiyingx@ucar.edu>
2024-10-22 15:21:54 -0600

    modify to copy cpu log to cpu directory

--- ncar_scripts/derecho_a100_coverage.sh
+++ ncar_scripts/derecho_a100_coverage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #PBS -N SAMURAI
 #PBS -A NEOL0013
-#PBS -l select=1:ncpus=128:ompthreads=128:mem=230GB
-#PBS -q main
+#PBS -l select=1:ncpus=64:ompthreads=1:mem=300GB:ngpus=1
+#PBS -q develop
 #PBS -l walltime=02:30:00
 #PBS -j oe
 #PBS -k eod
@@ -11,26 +11,32 @@ cd $PBS_O_WORKDIR
 cd ..
 export SAMURAI_ROOT=$(pwd)
 
-echo $SAMURAI_ROOT
-
 ID=`date '+%Y%m%d%H%M'`
+
+sed -i 's/cc70/cc80/g' CMakeLists.txt
+sed -i 's/cc90/cc80/g' CMakeLists.txt
+
 ##################
 # Build the code #
 ##################
+sed -i 's/cc70/cc80/g' CMakeLists.txt
+sed -i 's/cc90/cc80/g' CMakeLists.txt
+
 cd ncar_scripts 
-./ncar_build.sh
+./ncar_build.sh gpu
 
 ##############
 # Run a case #
 ##############
-suffix="derecho_cpu"
+suffix="derecho_gpu"
 for i in beltrami supercell hurricane typhoonChanthu2020 # hurricane_4panel
 do
+
   ./ncar_run.sh $SAMURAI_ROOT/ncar_scripts/TDRP/${i}.tdrp >& log_${i}_$suffix.$ID
-  echo "log_${i}_$suffix.$ID is done"
   if [ ! -d  ${i}_${suffix} ]; then
      mkdir ${i}_${suffix}
   fi
+  echo "log_${i}_$suffix.$ID is done"
   mv $SAMURAI_ROOT/run/timing.0 ${i}_${suffix}/timing.$ID
-  mv $SAMURAI_ROOT/run/samurai* log*${suffix}* ${i}_${suffix}
+  mv $SAMURAI_ROOT/run/samurai* log* ${i}_${suffix}
 done
